ec17a7298b6077a5ec217e70739c57c4bdbb2e8a
Sudhakar Venkatraman <sudhakar.venkatraman@omnipresence.io>
2022-04-12 23:11:17 +0530

    t changes

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -8,6 +8,7 @@ import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
+    private t = '8d86349c09eb499d9cf2f9ed07f4b402dc85c7972b25350a7253f7a82d28ad97';
     intercept(
         req: HttpRequest<any>,
         next: HttpHandler
@@ -15,7 +16,7 @@ export class TokenInterceptor implements HttpInterceptor {
         req = req.clone({
             headers: req.headers.set(
                 "Authorization",
-                "Bearer 8d86349c09eb499d9cf2f9ed07f4b402dc85c7972b25350a7253f7a82d28ad97"
+                `Bearer ${this.t}`
             )
         });
         return next.handle(req);
