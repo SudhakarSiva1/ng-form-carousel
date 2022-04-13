c5a96fdd351d9cb9731841a139099352bd68f917
Sudhakar Venkatraman <sudhakar.venkatraman@omnipresence.io>
2022-04-13 22:46:47 +0530

    carousel changes

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -5,11 +5,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent {
 
-  constructor() { }
+  private images: any = [1, 2, 3, 4, 5, 6, 7, 8, 9].map((v) => ({ name: `Image ${v}`, url: `https://picsum.photos/900/500?random&t=${Math.random()}` }));
+  public currentList: any = [];
+  public currentIndex: number = 0;
+  public splicedImages:any = [];
+  public totalItems: number = 0;
 
-  ngOnInit(): void {
+  constructor() {
+    this.totalItems = this.images.length;
+    for (let index = 0; index < this.images.length; index + 4) {
+      this.splicedImages.push(this.images.splice(index, 4));
+    }
+    this.currentList = this.splicedImages[0];
+  }
+
+  public prev() {
+    if (this.currentIndex == 0) return;
+    this.currentIndex -= 1;
+    this.currentList = this.splicedImages[this.currentIndex];
+  }
+
+  public next() {
+    if (this.currentIndex == this.splicedImages.length - 1) return;
+    this.currentIndex += 1;
+    this.currentList = this.splicedImages[this.currentIndex];
   }
 
 }
